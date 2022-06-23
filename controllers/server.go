67c64ab6a6e4c656a6dc4ff1eb42287e416b02ee
dooster <dustin2burns@yahoo.com>
2022-06-23 14:50:09 -0500

    seeded data and inserted get route

--- controllers/server.go
+++ controllers/server.go
@@ -10,7 +10,8 @@ import (
 func StartServer() {
 	router := mux.NewRouter()
 	router.HandleFunc("/test", test)
-
+	router.HandleFunc("/sensors/{sensor_id:[0-9]+}/thresholds", postId).Methods(http.MethodPost)
+	router.HandleFunc("/sensors/{sensor_id:[0-9]+}/thresholds/{threshold_id: [0-9]+}", getSensorThreshold).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
 
 }
